d2d35b1a54592e33fad32844c781653f1e46a063
pedrordgs <pedrombr99@gmail.com>
2020-07-21 21:26:18 +0100

    updated

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PKG="sudo pacman -S"
-OPT="--needed --noconfirm"
+# ignored files list
+declare -a ignore_list=(".git"
+                        ".gitignore"
+                        "README.md"
+                        "scripts"
+                    )
 
-
-$PKG dunst $OPT
-$PKG i3 $OPT
-$PKG neovim $OPT
-$PKG polybar $OPT
-$PKG ranger $OPT
-$PKG rofi $OPT
-$PKG git $OPT
-
-$PKG zsh $OPT
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-chsh -s "$(command -v zsh)"
-
-~/dotfiles/linking.sh
+# go throw all files except ignore list
+for file in ~/.dotfiles/*; do
+  if [ -d ${file} ] && [[ ! ${file} =~ ${ignore_list[@]} ]]; then
+    echo "Installing $(basename file)"
+    sudo pacman -S $(basename file) --needed --noconfirm
+    stow $(basename $file)
+    if $SCRIPT_DEBUG; then echo "$(basename $file) stowed."; fi
+  fi
+done
