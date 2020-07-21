995e81825def080bcb4dd2b9db7fd540168d60f6
pedrordgs <pedrombr99@gmail.com>
2020-07-21 23:17:59 +0100

    updated

--- install.sh
+++ install.sh
@@ -10,9 +10,41 @@ declare -a ignore_list=(".git"
 # go throw all files except ignore list
 for file in ~/.dotfiles/*; do
   if [ -d ${file} ] && [[ ! ${file} =~ ${ignore_list[@]} ]]; then
-    echo "Installing $(basename file)"
-    sudo pacman -S $(basename file) --needed --noconfirm
+    echo "Installing $(basename $file)"
+    sudo pacman -S $(basename $file) --needed --noconfirm
     stow $(basename $file)
     if $SCRIPT_DEBUG; then echo "$(basename $file) stowed."; fi
   fi
 done
+
+# Install vundle and vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+nvim +PluginInstall +qall
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# change shell to zsh
+chsh -s $(which zsh)
+
+# install all dependencies
+while read line;
+do
+    echo "Installing $line"
+    sudo pacman -S $line --needed --noconfirm
+done
+
+# install fonts
+echo "Installing Cascadia Code"
+sudo pacman -S ttf-cascadia-code --needed --noconfirm
+echo "Installing Ionicons"
+sudo pacman -S ttf-ionicons --needed --noconfirm
+echo "Installing Font Awesome"
+sudo pacman -S ttf-font-awesome --needed --noconfirm
+
---
+++ system/.zprofile
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# add scripts to $PATH
+export PATH="~/.scripts"
+
+export EDITOR="nvim"
+export TERMINAL="termite"
+export BROWSER="brave"
+export READER="zathura"
+
+
